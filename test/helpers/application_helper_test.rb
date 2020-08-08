ed5ef4e0020508b470782063011b0a6aba4ee0a8
etrimbs <eric.trimo@gmail.com>
2020-08-08 12:06:15 -0400

    Change website base title to Eric Trimble

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
     
     test "full title helper" do
-        assert_equal full_title, 'Ruby on Rails Tutorial Sample App'
-        assert_equal full_title("Help"), 'Help | Ruby on Rails Tutorial Sample App'
+        assert_equal full_title, 'Eric Trimble'
+        assert_equal full_title("Help"), 'Help | Eric Trimble'
     end
     
 end
